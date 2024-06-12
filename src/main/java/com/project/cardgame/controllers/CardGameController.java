cbe1a2b821af9a35ef33ea5396e7cb559b0c04a8
Hamzah Sheikh <hamzah_sheikh@outlook.com>
2024-06-12 12:53:57 -0400

    Added endpoint for "Create Game" operation

--- src/main/java/com/project/cardgame/controllers/CardGameController.java
+++ src/main/java/com/project/cardgame/controllers/CardGameController.java
@@ -1,11 +1,18 @@
 package com.project.cardgame.controllers;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.cardgame.Game;
+
 @RestController
 public class CardGameController {
+
+    private Game game;
 
 
     // Test endpoint
@@ -13,6 +20,23 @@ public class CardGameController {
     public String test() {
 
         return "This is the Card Game REST API! Hello World!";
+    }
+
+    /**
+     * Create a new game if one does not already exist
+     * 
+     * @return 200 OK if the game was created, 400 BAD REQUEST if a game already exists
+     */
+    @PostMapping(value = "/create/game")
+    public ResponseEntity<Object> createGame() {
+
+        if (game != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        game = Game.getInstance();
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }

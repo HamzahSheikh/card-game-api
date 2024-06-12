e5d110363d33c7c52c1d3e9465b2be541255ab54
Hamzah Sheikh <hamzah_sheikh@outlook.com>
2024-06-12 14:48:02 -0400

    Added List of Cards to Player class

--- src/main/java/com/project/cardgame/Player.java
+++ src/main/java/com/project/cardgame/Player.java
@@ -1,15 +1,28 @@
 package com.project.cardgame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
     private String name;
+    private List<Card> hand;
 
     public Player(String name) {
         this.name = name;
+        this.hand = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public void addToHand(Card card) {
+        hand.add(card);
+    }
+
+    public List<Card> getHand() {
+        return hand;
     }
    
 }

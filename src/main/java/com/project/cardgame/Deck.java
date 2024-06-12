cdde7718d27dcf85a004e62a5e2b7f207e91f709
Hamzah Sheikh <hamzah_sheikh@outlook.com>
2024-06-12 13:37:57 -0400

    Added list of Cards to Deck class, method to merge decks

--- src/main/java/com/project/cardgame/Deck.java
+++ src/main/java/com/project/cardgame/Deck.java
@@ -1,11 +1,32 @@
 package com.project.cardgame;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Deck {
 
+    private List <Card> cards;
+
     public Deck() {
 
+        cards = new ArrayList<>();
+
+        //for every suit of cards declared in CardSuit enum, we add all values of cards declared in CardValue enum
+        for (CardSuit suit : CardSuit.values()) {
+            for (CardValue value : CardValue.values()) {
+                cards.add(new Card(suit, value));
+            }
+        }
+
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    // add another deck of cards to the current deck
+    public void mergeDecks(Deck deck) {
+        this.cards.addAll(deck.getCards());
     }
   
-    
 }

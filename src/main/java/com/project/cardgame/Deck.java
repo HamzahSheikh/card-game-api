3f7451d71420707287aada5e4f48992edb71502d
Hamzah Sheikh <hamzah_sheikh@outlook.com>
2024-06-12 15:16:42 -0400

    Added Map to Deck class to record count per card suit type

--- src/main/java/com/project/cardgame/Deck.java
+++ src/main/java/com/project/cardgame/Deck.java
@@ -1,21 +1,28 @@
 package com.project.cardgame;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Deck {
 
     private List <Card> cards;
+    private Map<CardSuit, Integer> suitCount;
 
     public Deck() {
 
         cards = new ArrayList<>();
+        suitCount = new HashMap<>();
 
         //for every suit of cards declared in CardSuit enum, we add all values of cards declared in CardValue enum
         for (CardSuit suit : CardSuit.values()) {
             for (CardValue value : CardValue.values()) {
                 cards.add(new Card(suit, value));
             }
+
+            //add the number of cards in each suit to the suitCount map
+            suitCount.put(suit, CardValue.values().length);
         }
 
     }
@@ -27,6 +34,11 @@ public class Deck {
     // add another deck of cards to the current deck
     public void mergeDecks(Deck deck) {
         this.cards.addAll(deck.getCards());
+
+        //update the suit count for each suit in the deck
+        for(CardSuit suit : CardSuit.values()) {
+            suitCount.put(suit, suitCount.get(suit) + deck.suitCount.get(suit));
+        }
     }
 
     public void shuffle() {
@@ -42,7 +54,15 @@ public class Deck {
     public Card dealCard() {
         // Remove the last card from the deck to ensure O(1) time complexity
         Card card = cards.remove(cards.size() - 1);
+
+        // Decrement the suit count for the suit of the card that was dealt
+        suitCount.put(card.getSuit(), suitCount.get(card.getSuit()) - 1);
         return card;
+    }
+
+    // get the suit count of the deck
+    public Map<CardSuit, Integer> getSuitCount() {
+        return suitCount;
     }
   
 }

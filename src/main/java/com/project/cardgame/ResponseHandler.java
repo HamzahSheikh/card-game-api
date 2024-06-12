04a33be287d232166a3f6a1eace3363878b26b68
Hamzah Sheikh <hamzah_sheikh@outlook.com>
2024-06-12 13:58:54 -0400

    Created ResponseHandler class

---
+++ src/main/java/com/project/cardgame/ResponseHandler.java
@@ -0,0 +1,46 @@
+package com.project.cardgame;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseHandler {
+
+    public static ResponseEntity<Object> responseBuilder(String message, Object data, HttpStatus httpStatus) {
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", message);
+        response.put("data", data);
+        response.put("status", httpStatus.value());
+
+        return new ResponseEntity<>(response, httpStatus);
+    }
+
+    public static ResponseEntity<Object> responseBuilder(String message, HttpStatus httpStatus) {
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", message);
+        response.put("status", httpStatus.value());
+
+        return new ResponseEntity<>(response, httpStatus);
+    }
+
+    public static ResponseEntity<Object> noGamEntity() {
+        return responseBuilder("No game created!", HttpStatus.BAD_REQUEST);
+    }
+
+    public static ResponseEntity<Object> gameExistsEntity() {
+        return responseBuilder("Game already exists!", HttpStatus.BAD_REQUEST);
+    }
+
+    public static ResponseEntity<Object> noGameDeckEntity() {
+        return responseBuilder("No Game Deck!", HttpStatus.BAD_REQUEST);
+    }
+
+    public static ResponseEntity<Object> noReserveDeckEntity() {
+        return responseBuilder("No reserve deck created!", HttpStatus.BAD_REQUEST);
+    }
+ 
+}
